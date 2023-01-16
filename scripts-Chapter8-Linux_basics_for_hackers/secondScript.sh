9d2c7ecce2cd65ccf823ad11d16c0c39b8825e78
Cristopher Vanegas <cristopher.platzi@gmail.com>
2023-01-16 12:23:57 -0500

    ADDED scripts exercises from Chapter 8 from book linux basics for hackers

---
+++ scripts-Chapter8-Linux_basics_for_hackers/secondScript.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This is my second bash script. In this one, I prompt
+# the user for input, place the input in a variable, and
+# display the variable contents in a string.
+
+echo "What is your name?   > "
+read name
+
+echo "What chapter are you on in Linux for Hackers?   > "
+read chapter
+
+echo "Welcome $name to Chapter $chapter of Linux Basics for Hackers!"
